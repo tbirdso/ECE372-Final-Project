872cb41b440bf70acad1ba68a50d7717f40015cc
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-04-05 09:34:59 -0400

    Add Files
    Added Unity scene
    Added resource files

---
+++ ECE372 Final Project/Assets/Scripts/ArdEventComms.cs
@@ -0,0 +1,42 @@
+﻿/* By Tom Birdsong
+ * Last Updated 3/2/2019
+ * 
+ * Using Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+using System.Collections;
+using UnityEngine;
+
+public abstract class ArdEventComms : MonoBehaviour {
+
+	public delegate void PulseEventHandler(bool val);
+	public event PulseEventHandler Pulse;
+
+	public void OnConnectionEvent(bool connected) {
+		if (connected) {
+			Debug.Log("Connection established");
+		} else {
+			Debug.Log("Connection attempt failed or disconnection detected");
+		}
+	}
+
+	public virtual void OnMessageArrived(string message) {
+		Debug.Log ("Incoming message: " + message);
+
+		if (message == "0") {
+			OnPulse ();
+		}
+	}
+
+	public void OnPulse() {
+
+		PulseEventHandler handler = Pulse;
+
+		if (handler != null)
+			handler.Invoke (true);
+	}
+
+}
---
+++ ECE372 Final Project/Assets/Scripts/RGBComm.cs
@@ -0,0 +1,74 @@
+﻿/**
+ * Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using UnityEngine;
+using System.Collections;
+
+/**
+ * Sample for reading using polling by yourself, and writing too.
+ */
+public class RGBComm : BasicArdComm
+{
+	// Initialization
+	protected override void Start()
+	{
+		base.Start ();
+
+		Debug.Log("Press A or Z to execute some actions");
+	}
+
+	// Executed each frame
+	protected override void Update()
+	{
+		//---------------------------------------------------------------------
+		// Send data
+		//---------------------------------------------------------------------
+
+		// If you press one of these keys send it to the serial device. A
+		// sample serial device that accepts this input is given in the README.
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			Debug.Log("Sending #");
+			serialController.SendSerialMessage("#");
+		}
+
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			Debug.Log ("Sending #FF0000");
+			serialController.SendSerialMessage ("#FF0000#");
+		}
+
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Debug.Log ("Sending #00FFFF");
+			serialController.SendSerialMessage ("#00FFFF#");
+		}
+
+		if (Input.GetKeyDown (KeyCode.K)) {
+			Debug.Log ("Sending #FFFF00");
+			serialController.SendSerialMessage ("#FFFF00");
+		}
+
+		if (Input.GetKeyDown (KeyCode.L)) {
+			Debug.Log ("Let there be light");
+			serialController.SendSerialMessage ("#FFFFFF");
+		}
+
+		//---------------------------------------------------------------------
+		// Receive data
+		//---------------------------------------------------------------------
+
+		base.Update ();
+	}
+
+	public string makeByteStr(byte val) {
+		char[] cArr = new char[2];
+		cArr [0] = (char)val;
+		cArr [1] = '\0';
+		return cArr.ToString ();
+	}
+}
