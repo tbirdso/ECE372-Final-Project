54955ade4d1f91b9cf60b1b33544a0705074ca1e
tbirdso <40648863+tbirdso@users.noreply.github.com>
2019-04-08 01:26:57 -0400

    TX/RX Updates
    -Added base Unity message listener
    -Implemented PIC32 RX
    -PIC32 INT0
    -PIC32 LED
    -PIC32 A0 and A1

---
+++ ECE372 Final Project/Assets/Scripts/PICMessageListener.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PICMessageListener : MonoBehaviour {
+
+    public delegate void JoystickHandler(string s,float f);
+    public event JoystickHandler JoystickMove;
+
+    public delegate void ButtonHandler(int i,bool b);
+    public event ButtonHandler ButtonMove;
+
+    public delegate void TemperatureHandler(float f);
+    public event TemperatureHandler TemperatureRead;
+
+    public void OnConnectionEvent(bool connected) {
+		Debug.Log ("Device " + (connected ? "connected" : "disconnected") + ".");
+	}
+
+	public void OnMessageArrived(string message) {
+		//TODO: parse and execute
+		char s1 = message[0];
+		char s2 = message[1];
+		string rest = message.Substring (2);
+
+		switch (s1) {
+            case 't':
+            case 'T':
+                OnTemperatureRead((float)s2);
+                break;
+		case 'P':
+		case 'p':
+			Print (rest);
+			break;
+		case 'j':
+		case 'J':
+                OnJoystickMove(s2, message[3]);
+			break;
+		case 'b':
+		case 'B':
+                OnButtonMove(s2, (message[3] = '1' ? true : false));
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	public void Print(string val) {
+		Debug.Log("Received: " + val);
+	}
+
+    #region Event Handlers
+    public void OnTemperatureRead(int t)
+    {
+        TemperatureHandler handler = TemperatureRead;
+
+        float temp = (float)t;
+
+        if(handler != null)
+        {
+            handler.Invoke(temp);
+        }
+    }
+
+    public void OnJoystickMove(string axis, float val)
+    {
+        JoystickHandler handler = JoystickMove;
+
+        if(handler != null)
+        {
+            handler.Invoke(axis,val);
+        }
+    }
+
+    public void OnButtonMove(int id, bool pressed)
+    {
+        ButtonHandler handler = ButtonMove;
+
+        if(handler != null)
+        {
+            handler.Invoke(id, pressed);
+        }
+    }
+
+    #endregion
+
+}
--- UnityInterface.X/main.c
+++ UnityInterface.X/main.c
@@ -8,41 +8,71 @@
 
 #include <plib.h>
 
+/* PIN DIAGRAM
+ * 5    - U1RX
+ * 18   - Analog0 (** NOT 5V TOLERANT **)
+ * 19   - Analog1 (** NOT 5V TOLERANT **)
+ * 22   - LED
+ * 23   - U1TX
+ * 37   - INT0
+ * 40   - GND
+ */
+
 void setupPins();
+void setupInterrupts();
+void setupADC();
 void setupUART();
 void makeMessage(char*,char,char,int*,char*);
 void sendMessage(char*,int);
 
 void OnINT0();
-void OnTimer();
 void OnRXDone();
 void OnTXReady();
 void OnADCReady();
 void printbyte(char);
 
 const int BAUD_RATE = 9600;
+const int MAX_REC_BUF_LEN = 8;
+unsigned char buttonEdgeDir = 1; //default to rising edge detection
+
+char recbuf[8] = {0,0,0,0,0,0,0,0};
+char recbuflen = 0;             //default to empty buffer
+int bufReady = 0;               //flag controlled by SW
 
 int joystick_x = 0;
 int joystick_y = 0;
 float temp = 0;
 
 
+
 main() {
     
     setupPins();
+    setupInterrupts();
+    setupADC();
     setupUART();
     
-    char params[2] = {'a','b'};
+    //Test Transmission
+    /*
+    char params[2] = {'a','x'};
     int count[1] = {2};
     char sel1 = '\n';
     char sel2 = '0';
     char out[4] = {0,0,0};
     makeMessage(out,sel1,sel2,count,params);
     sendMessage(out,*count);
-    
+    */
+     
+     
     while(1) {
         
-        sendMessage(out,*count);
+        //sendMessage(out,*count);
+        
+        if(bufReady) {
+            PORTBbits.RB2 = 1;
+            recbuflen = 0;
+            bufReady = 0;
+        }
     }
     
 }
@@ -60,7 +90,61 @@ void setupPins() {
     
     PPSOutput(1, RPB3, U1TX);
     PPSInput(3, U1RX, RPC6);
-};
+}
+
+void setupInterrupts() {
+    
+   INTEnableSystemMultiVectoredInt();  //handle interrupts
+    
+    //INT0
+    TRISBbits.TRISB7 = 1;
+    
+    INTCONbits.INT0EP = buttonEdgeDir;  //rising edge
+    IEC0bits.INT0IE = 1;    //enable
+    IPC0bits.INT0IP = 1;    //priority 1
+    
+    //U1RX
+    IFS1bits.U1RXIF = 0;
+    IPC8bits.U1IP = 2;
+    IPC8bits.U1IS = 1;
+    IEC1bits.U1RXIE = 1;
+    
+    //AD1
+    IEC0bits.AD1IE = 1;
+    IPC5bits.AD1IP = 1;
+    IPC5bits.AD1IS = 1;
+    IFS0bits.AD1IF = 0;
+    
+}
+
+void setupADC() {
+    //TODO: configure to use A0 and A1, not auto-sample
+    
+    //configure ADC
+    //table 17.4
+    AD1CHSbits.CH0SB = 0; //select scan
+    //AD1CHSbits.CH0SA = 0; //select A0
+    AD1CON1bits.FORM = 0; //use 16-bit integer output
+    AD1CON1bits.SSRC = 7; //auto convert
+    
+    //scan channels A0 and A1
+    AD1CSSL = 0x0003;
+    
+    AD1CON2bits.VCFG = 0; //use built-in VR+ and VR-
+    AD1CON2bits.CSCNA = 1; //Do scan
+    AD1CON2bits.SMPI = 3; //interrupt at completion of 4 conversions
+    AD1CON2bits.BUFM = 0; //buffer configured as one 16-word buffer
+    
+    AD1CON2bits.ALTS = 0; //always use MUX A (don't alternate))
+    AD1CON3bits.SAMC = 8; //auto sample at 8 TADs
+    AD1CON3bits.ADRC = 0; //ADC conversion clock derived from PBCLK
+    AD1CON3bits.ADCS = 2; //ADC conversion clock: 8*TPB = TAD
+    
+    AD1CON1bits.ASAM = 1; //Sample auto-start
+    
+    AD1CON1bits.ON = 0; //FIXME
+    
+}
 
 void setupUART() {
     OSCCONbits.PBDIV = 3;   //PBCLK is SYSCLK divided by 8
@@ -76,7 +160,7 @@ void setupUART() {
                         // (source: PIC32 Datasheet - 21)
     
     U1STASET = 0x1400;  // Enable Transmit and Receive
-};
+}
 
 void makeMessage(char* out, char s1,char s2,int* len, char* params) {
     int i = 0;
@@ -115,11 +199,77 @@ void sendMessage(char* message, int len) {
         
 }
 
-void OnINT0();
-void OnTimer();
-void OnRXDone();
+void __ISR(3) OnINT0() {
+    char params[1];
+    params[0] = INTCONbits.INT0EP + 65;
+    int count[1] = {1};
+    char sel1 = 'b';
+    char sel2 = '1';
+    char out[5];
+    makeMessage(out,sel1,sel2,count,params);
+    sendMessage(out,*count);
+
+    INTCONbits.INT0EP = (INTCONbits.INT0EP == 0) ? 1 : 0;  //toggle edge
+    
+    IFS0bits.INT0IF = 0;   
+}
+
+void __ISR(32) OnRXDone() {
+    char rx_byte = 0;
+    if((rx_byte = U1RXREG) != 0) {
+                
+        if(recbuflen >= MAX_REC_BUF_LEN) {
+            bufReady = 1;
+        } else {
+            recbuf[recbuflen] = rx_byte;
+            recbuflen++;
+        }
+        
+        bufReady = (recbuflen >= MAX_REC_BUF_LEN) ? 1 : 0;
+        
+        //test receive
+            char params[1];
+        params[0] = recbuflen+65;
+        int count[1] = {3};
+        char sel1 = 'r';
+        char sel2 = 'x';
+        char out[5];
+        makeMessage(out,sel1,sel2,count,params);
+        sendMessage(out,*count);
+    }
+                        
+    IFS1bits.U1RXIF = 0;  
+}
 void OnTXReady();
-void OnADCReady();
+
+void __ISR(23) OnADCReady() {
+    unsigned int samples[8] = {0,0,0,0,0,0,0,0};
+
+    samples[0] = ADC1BUF0;
+    samples[1] = ADC1BUF1;
+    samples[2] = ADC1BUF2;
+    samples[3] = ADC1BUF3;
+
+    
+    int mean = 0, i = 0;
+    for( ; i<4; i++) {
+        mean += (samples[i] >>2);
+    }
+    mean /= 8;
+    
+    //test input
+    char params[1];
+    params[0] = (int)mean;
+    int count[1] = {1};
+    char sel1 = 'j';
+    char sel2 = 'x';
+    char out[5];
+    makeMessage(out,sel1,sel2,count,params);
+    sendMessage(out,*count);
+    
+    IFS0bits.AD1IF = 0;
+    
+}
 
 void printbyte(char b) {
     LATC = b;
